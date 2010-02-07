69821da061aa45fb6329717545d6c9630118b248
Avaera8820 <Avaera8820@484f407c-ffe4-11de-b631-23daf5ca4ffa>
2010-02-07 00:01:38 +0000

    Added note in English, modified possible command for turning.

--- AutonomousZone2.java
+++ AutonomousZone2.java
@@ -36,17 +36,17 @@ public class AutonomousZone2 {
 	/*
 	In ENGLISH:
 	
-	Wait (.5 sec)          .5
-	Go forward (1 sec)    1.5
+	Wait (.5 sec)                   .5
+	Go forward (1 sec)             1.5
 	Stop
-	Kick (1.5 sec)        3.0
-	Rotate Right (1 sec)  4.0
-	Go forward (1 sec)    5.0
+	Kick (1.5 sec)                 3.0
+	Rotate Right (90 deg) (1 sec)  4.0
+	Go forward (1 sec)             5.0
 	Stop
-	Rotate Left (1 sec)   6.0
-	Go forward (1 sec)    7.0
+	Rotate Left (90 deg) (1 sec)   6.0
+	Go forward (1 sec)             7.0
 	Stop
-	Kick (1.5 sec)        8.5
+	Kick (1.5 sec)                 8.5
 	End
 	*/
 	
@@ -61,14 +61,14 @@ public class AutonomousZone2 {
 			Kicking.kickBall();
 		}
 		else if(timerValue<4.0){
-			//Rotate right - use Hardware.robotDrive.driveAtSpeed(0.0,1.0)?
+			//Rotate right - use Hardware.robotDrive.driveAtSpeed(-1.0,1.0)?
 		}
 		else if(timerValue<5.0){
 			Hardware.robotDrive.driveAtSpeed(1.0,1.0);
 		}
 		else if(timerValue<6.0){
 			Hardware.robotDrive.stop();
-			//Rotate left - use Hardware.robotDrive.driveAtSpeed(1.0,0.0)?
+			//Rotate left - use Hardware.robotDrive.driveAtSpeed(1.0,-1.0)?
 		}
 		else if(timerValue<7.0){
 			Hardware.robotDrive.driveAtSpeed(1.0,1.0);
